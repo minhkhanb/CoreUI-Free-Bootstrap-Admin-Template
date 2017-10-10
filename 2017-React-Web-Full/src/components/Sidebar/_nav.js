fffeeea5c065e65d49c097ba58e11f03dbed3280
KHALE\Administrator <minhkhanb@gmail.com>
2017-10-10 17:04:43 +0700

    add sidebar

--- 2017-React-Web-Full/src/components/Sidebar/_nav.js
+++ 2017-React-Web-Full/src/components/Sidebar/_nav.js
@@ -1,13 +1,34 @@
 export default {
-  items: [
-    {
-      name: 'Dashboard',
-      url: '/dashboard',
-      icon: 'icon-speedometer',
-      badge: {
-        variant: 'info',
-        text: 'NEW'
-      }
-    }
-  ]
+    items: [
+        {
+            name: 'Dashboard',
+            url: '/dashboard',
+            icon: 'icon-speedometer',
+            badge: {
+                variant: 'info',
+                text: 'NEW'
+            }
+        },
+        {
+            title: true,
+            name: 'Cây Cảnh',
+            wrapper: {            // optional wrapper object
+                element: "span",      // required valid HTML5 element tag
+                attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+            },
+            class: ""             // optional class names space delimited list for title item ex: "text-center"
+        },
+        {
+            name: 'Hoa',
+            url: '/flowers',
+            icon:'icon-puzzle',
+            children: [
+                {
+                    name: 'Hoa Hồng',
+                    url: '/components/flowers/rose',
+                    icon: 'icon-puzzle'
+                }
+            ]
+        }
+    ]
 };
--- 2017-React-Web-Full/webpack.config.js
+++ 2017-React-Web-Full/webpack.config.js
@@ -14,97 +14,102 @@ console.log('BUILD_DIR', BUILD_DIR);
 console.log('SRC_DIR', SRC_DIR);
 
 module.exports = {
-  entry: {
-    index: [SRC_DIR + '/index.js']
-	},
-  output: {
-    path: BUILD_DIR,
-    filename: '[name].bundle.js'
-  },
-  watch: true,
-  devServer: {
-    contentBase: BUILD_DIR,
-    //   port: 9001,
-    compress: true,
-    hot: true,
-    open: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            presets: ['react', 'env']
-          }
-        }
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        test: /\.(scss)$/,
-        use: ['css-hot-loader'].concat(extractSCSS.extract({
-          fallback: 'style-loader',
-          use: [
+    entry: {
+        index: [SRC_DIR + '/index.js']
+    },
+    output: {
+        path: BUILD_DIR,
+        filename: '[name].bundle.js'
+    },
+    watch: true,
+    devServer: {
+        contentBase: BUILD_DIR,
+        //   port: 9001,
+        compress: true,
+        hot: true,
+        open: true
+    },
+    module: {
+        rules: [
             {
-              loader: 'css-loader',
-              options: { alias: { '../img': '../public/img' } }
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ['react', 'env']
+                    }
+                }
             },
             {
-              loader: 'sass-loader'
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(scss)$/,
+                use: ['css-hot-loader'].concat(extractSCSS.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {alias: {'../img': '../public/img'}}
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                }))
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|ico)$/,
+                use: [
+                    {
+                        // loader: 'url-loader'
+                        loader: 'file-loader',
+                        options: {
+                            name: './img/[name].[hash].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: './fonts/[name].[hash].[ext]'
+                }
+            }]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.NamedModulesPlugin(),
+        extractCSS,
+        extractSCSS,
+        new HtmlWebpackPlugin(
+            {
+                inject: true,
+                template: './public/index.html'
             }
-          ]
-        }))
-      },
-      {
-        test: /\.css$/,
-        use: extractCSS.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
+        ),
+        new CopyWebpackPlugin([
+                {from: './public/img', to: 'img'}
+            ],
+            {copyUnmodified: false}
+        ),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
         })
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        use: [
-          {
-            // loader: 'url-loader'
-            loader: 'file-loader',
-            options: {
-              name: './img/[name].[hash].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
-        options: {
-          name: './fonts/[name].[hash].[ext]'
-        }
-      }]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.NamedModulesPlugin(),
-    extractCSS,
-    extractSCSS,
-    new HtmlWebpackPlugin(
-      {
-        inject: true,
-        template: './public/index.html'
-      }
-    ),
-    new CopyWebpackPlugin([
-        {from: './public/img', to: 'img'}
-      ],
-      {copyUnmodified: false}
-    )
-  ]
+    ]
 }
 ;
